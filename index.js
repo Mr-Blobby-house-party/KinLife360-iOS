1e44098bc3a5cdde8cabfc9d68af35bebffbe0f2
Mr-Blobby-house-party <christian@cundall.org>
2026-04-29 22:01:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function parseLocationMappings() {
 const LOCATION_MAPPINGS = parseLocationMappings();
 
 function findNearestLocation(lat, lon) {
-  const threshold = 0.003; // ~300m radius
+  const threshold = 50; // ~300m radius
   
   for (const loc of LOCATION_MAPPINGS) {
     const distance = Math.sqrt(
